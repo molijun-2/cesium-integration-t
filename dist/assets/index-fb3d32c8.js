c0d18d95cc78f3b5c4f3ae11c2ca7266afb24f6b
hbn <lauraceae@foxmail.com>
2023-02-24 16:57:39 +0800

    1

--- dist/assets/index-fb3d32c8.js
+++
@@ -1,1 +0,0 @@
-import{C as n}from"./index-cbd8dea0.js";import{b as r,c,I as t,f as a,H as o}from"./index-e5a4cfa6.js";const p={__name:"index",setup(u){return(_,s)=>{const e=n;return r(),c("div",null,[t(" 铠甲勇士 "),a(e,{type:"primary"},{default:o(()=>[t("Primary Button")]),_:1}),a(e,null,{default:o(()=>[t("Default Button")]),_:1})])}}};export{p as default};
